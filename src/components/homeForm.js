211bc8d693f2cc68bbf95d9cc6c5e38359e20d47
komalcoding <komalcoding@gmail.com>
2021-12-21 18:12:10 +0530

    map used student jason

--- src/components/homeForm.js
+++ src/components/homeForm.js
@@ -8,7 +8,7 @@ function HomeForm() {
     const submitButtonStyle ={margin:'5px', padding:'3px',fontSize:'25px', backgroundColor:'green', borderRadius:'8px'};
 
 const empty_Student = { 
-  name:'abc',
+  name:'',
   email:'',
   enrollmentNo : '',
   mobile: '',
@@ -18,67 +18,45 @@ const empty_Student = {
 
     const [student, setStudent]=useState(empty_Student);
 
-    const updateHandler = ()=>{        
-      var student2={...student, mobile:'+1 98654425222'};
+    const updateHandler = ()=>{  
+      var student2={...student, mobile:'+1 98654425222', marks:96};
      setStudent(student2);
     }
 
-
-    const changeNameHandler = (event)=>{  
-
-      //var field =  event.target.name;
-
-      //var newValue =  event.target.value;  
-      // console.log('field:'+field);
-      // console.log('newValue:'+newValue);
-      var student2={...student, name:event.target.value };
-     setStudent(student2);
+    const resetHandler = ()=>{       
+     setStudent(empty_Student);
     }
 
-    const changeEmailHandler = (event)=>{        
-      var student2={...student, email:event.target.value};
+    const changeHandler = (event)=>{ 
+      console.log("field Name: "+event.target.name);      
+      console.log("field Value: "+event.target.value);    
+      var student2={...student, [event.target.name]:event.target.value};
+
+      // dont write this ===========>student= student2;
     setStudent(student2);
     }
-
-    const changeEnrollmentHandler = (event)=>{        
-      var student2={...student, enrollmentNo:event.target.value};
-    setStudent(student2);
-    }
-
-
-    const changeMobileHandler = (event)=>{        
-      var student2={...student, mobile:event.target.value};
-    setStudent(student2);
-    }
-    const changeMarksHandler = (event)=>{        
-      var student2={...student, marks:event.target.value};
-    setStudent(student2);
-    }
-
-    // const resetHandler = ()=>{     
-    //  setStudent(empty_Student);
-    // }
-
 
 
 
     return (
         <div className="homeFormStyle">
           <div style={{minHeight:'700px'}}>
-            <h1>Name: <input type="text" id="studentName" name='name' value={student.name} onChange={(event)=>{changeNameHandler(event)}}/></h1>
-             <h1>Email: <input type="text" id="studentEmail" name='email' value={student.email} onChange={(event)=>{changeEmailHandler(event)}}/></h1>
-            <h1>E. Number: <input type="text" id="studentEnrollment" name='enrollmentNo' value ={student.enrollmentNo}  onChange={(event)=>{changeEnrollmentHandler(event)}} /></h1>
-            <h1>Mobile:<input type="text" id="studentMobile" name='mobile' value={student.mobile}  onChange={(event)=>{changeMobileHandler(event)}}/></h1>
-            <h1>Marks:<input type="text" id="studentMarks" name='marks' value={student.marks}  onChange={(event)=>{changeMarksHandler(event)}}/></h1>
+            <h1>Name: <input type="text" id="studentName" name='name' value={student.name} onChange={(event)=>{changeHandler(event)}}/></h1>
+             <h1>Email: <input type="text" id="studentEmail" name='email' value={student.email} onChange={(event)=>{changeHandler(event)}}/></h1>
+            <h1>E. Number: <input type="text" id="studentEnrollment" name='enrollmentNo' value ={student.enrollmentNo}  onChange={(event)=>{changeHandler(event)}} /></h1>
+            <h1>Mobile:<input type="text" id="studentMobile" name='mobile' value={student.mobile}  onChange={(event)=>{changeHandler(event)}}/></h1>
+            <h1>Marks:<input type="text" id="studentMarks" name='marks' value={student.marks}  onChange={(event)=>{changeHandler(event)}}/></h1>
         {/* </div>
         <div style={{minHeight:'100px', margin:'10px'}}> */}
-            <input style={resetButtonStyle}    type="submit" value="Reset"  />
+            <input style={resetButtonStyle}    type="submit" value="Reset" onClick={resetHandler} />
             <input style={submitButtonStyle} type="submit" value="Save"/>
-            <input style={resetButtonStyle}  type="submit" value="Update" />
+            <input style={resetButtonStyle}  type="submit" value="Update" onClick={updateHandler} />
+
+            <button>Submit</button>
         </div>
       </div>
     )
 }
 
-export default HomeForm
+export default HomeForm ;
 
--- src/components/homeTable.js
+++ src/components/homeTable.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import Student from './student'
+import React from "react";
+import Student from "./student";
 
 function HomeTable() {
-    return (
-        <div className="homeTableStyle">
-        <Student name="Pragya" marks="91" mobile="+91 123452340" />
-        <Student name="Shivji" marks="92" mobile="+91 1234567890" />
-        <Student name="Radhe" marks="92" mobile="+91 654789123" />
-      </div>
-    )
+  var students = [
+    { name: "Pragya", marks: "91", mobile: "+91 123452340" },
+    { name: "Shivji", marks: "92", mobile: "+91 123452340" },
+    { name: "Radhe", marks: "93", mobile: "+91 123452340" },
+    { name: "Intekhab", marks: "94", mobile: "+91 234567740" },
+  ];
+
+  const studentList = students.map((student) =>  
+   <Student name={students[0].name} marks={students[0].marks} mobile={students[0].mobile}  />
+  ); 
+
+  return (
+    // <div className="homeTableStyle">      
+    //   <Student name={students[0].name} marks={students[0].marks} mobile={students[0].mobile} />
+    //   <Student name={students[1].name} marks={students[1].marks} mobile={students[1].mobile} />
+    //   <Student name={students[2].name} marks={students[2].marks} mobile={students[2].mobile} />
+    //   <Student name={students[3].name} marks={students[3].marks} mobile={students[3].mobile} />
+    // </div>
+<div className="homeTableStyle">
+  {studentList}
+</div>
+
+  );
 }
 
-export default HomeTable
+export default HomeTable;
